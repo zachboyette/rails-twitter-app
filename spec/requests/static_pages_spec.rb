4244bbbe03bb3ea0683f7ca862d2e7dfb558d3c0
Zach <jzboyette@gmail.com>
2014-05-18 15:25:50 -0500

    add title tests to controller spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Rails Twitter App')
 		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title('Rails Twitter App | Home')
+		end
 	end	
 
 	describe "Help page" do
@@ -16,6 +21,11 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content ('Help')
 		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('Rails Twitter App | Help')
+		end
 	end
 
 	describe "About page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content ('About Us')
+		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title ('Rails Twitter App | About Us')
 		end
 	end
 end
