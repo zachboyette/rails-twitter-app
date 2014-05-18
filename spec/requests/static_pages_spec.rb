28bbdec4315d8fd64ce4dcf974cd1e8db6975530
Zach <jzboyette@gmail.com>
2014-05-18 14:44:02 -0500

    add code to test contents of help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,12 @@ describe "Static pages" do
 			expect(page).to have_content('Rails Twitter App')
 		end
 	end	
+
+	describe "Help page" do
+
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content ('Help')
+		end
+	end
 end
